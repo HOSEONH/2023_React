fad7ab067bd42a0eb9bab5a2df478d73ee431e37
HS H <3262398@naver.com>
2023-03-30 18:17:03 +0900

    0330 수업 정리

--- ex-react/src/App.js
+++ ex-react/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import CountComp from './components'
-
+import PropsComp from './components/PropsComp';
+import CountComp from './components/CountComp';
+import CountPropsComp from './components/CountPropsComp';
 
 function App() {
   return (
     <div className="App">
       {/* props을 사용하는 클래스컴포넌트 */}
-      <PropsComp color="red">props값을 받아와서 글자색을 바꿉니다</PropsComp>
+      <PropsComp color="blue">props값을 받아와서 글자색을 바꿉니다</PropsComp>
       {/* state을 사용하는 클래스컴포넌트
         버튼을 클릭할 때마다 10씩 증가하는 컴포넌트 */}
       <CountComp />
--- ex-react/src/components/CountComp.jsx
+++ ex-react/src/components/CountComp.jsx
@@ -1,43 +1,25 @@
-import { CountComp } from 'react'
+import React, { Component } from 'react'
 
-class CountComp extends Component {
-    // state 컴포넌트 안에서 사용되는 변수
+export class CountComp extends Component {
     constructor(props){
-        // constructor를 사용하면 props를 따로 연결
         super(props);
-        // this.state안에 객체 형태로 값을 작성할 수 있다
+        this.state = {
+            count : 0
         }
     }
-    // 화면에 출력되는 html
-    // 가상돔을 화면에 출력하기 위해 render()를 호출
-    // 호출 시 render 안에서 사용한 변수는 초기화된다
-    // 초기화가 되지 않으려면 전역변수 또는 this.state사용
-    // 업데이트 마다 화면에 표시되는 값 : state, props
-    render(){
-        // this.state의 값을 비구조화 할당을 통해 사용
-        const {number, name} = this.state;
-        let count = 0;
-        // return의 html안에서 직접 불러와서 사용
-        return(
+    render() {
+        const {count} = this.state;
+        return (
             <div>
-                <h2>{name}</h2>
-                <h3>state의 값 {number}, 변수값 {count}</h3>
-                <h3>{this.state.number}</h3>
+                <h4>{count}</h4>
                 <button
-                    onClick={()=>{
-                        this.setState({number:number-1});
-                        this.setState({number:number-1});
-                        // 이전에 있던 state값을 prevState로 들고와서 수정
-                        // 값이 바뀐 후에 들고오기에 동기적으로 값이 수정
-                        // ()=>()의 형태로 사용하는 이유 : return으로 객체전달 위함
-                        // (prevState)=>({state속성:값})
-                        this.setState((prevState)=>({number:prevState.number+10}))
-                    }}
+                onClick={()=>{this.setState({count: count+10})}}
                 >
                     +10
                 </button>
             </div>
         )
     }
+}
 
-export default StateComp;
+export default CountComp
