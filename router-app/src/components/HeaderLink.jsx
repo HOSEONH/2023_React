78d3a902d41f9bda9bf946369bcb1d136dcecd6c
HS H <3262398@naver.com>
2023-04-12 15:17:21 +0900

    0412

---
+++ router-app/src/components/HeaderLink.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../CSS/headerlink.css'
+import { Link, NavLink } from 'react-router-dom'
+
+// 링크들을 모아서 헤더에 고정하기
+export default function HeaderLink() {
+    const fruit = "banana";
+
+    return (
+        <div>
+            {/* a태그 대신 컴포넌트의 주소로 이동 */}
+            <NavLink
+                className={({isActive})=>isActive ? "link-style":undefined}
+                to="/about">
+                About
+            </NavLink>
+            <br/>
+            {/* to의 속성값으로 자바스크립트의 문자열 가능 */}
+            <NavLink to={`/story/${fruit}`}
+                className={({isActive})=>isActive ? "link-style":undefined}
+                >
+                Story
+            </NavLink>
+            <NavLink to={`/articles`}
+                className={({isActive})=>isActive ? "link-style":undefined}
+                >
+                Articles
+            </NavLink>
+            <NavLink to="/story2"
+                className={({isActive})=>isActive ? "link-style":undefined}
+                >
+                Story2List
+            </NavLink>
+            <NavLink to="/navigate"
+                className={({isActive})=>isActive ? "link-style":undefined}
+                end // 주소가 완전히 일치할 때만 class이름 추가
+                >
+                Navigate
+            </NavLink>
+        </div>
+    )
+}
